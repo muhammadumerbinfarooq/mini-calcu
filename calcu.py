87d9787b4c4d85dae9a55ebcb337e04303611c78
muhammadumermemon <mumermemon312@gmail.com>
2024-10-25 23:16:42 +0500

    CODE TO PUBLIC

---
+++ calcu.py
@@ -0,0 +1,186 @@
+#---------------MATHS CALCULATOR------------------#
+"""def maths():
+  print("WELCOME TO THE EASY MATHS CALCULATOR !!")
+  print("WHICH MATHS CALCLCULATION YOU NEED TO PERFORM ? ")
+  print("1.ADDITION\n2.SUBTRACTION\n3.MULTIPLICATION\n4.DIVISION")
+
+
+resp1 = input("enter your operator here : ")
+if resp1 == "1":
+  def add(a=1,b=2,c=a+b):
+    c=int(a)+int(b)
+  print("THE SUM OF THE TWO NUMBERS IS : ",c)
+  
+
+  maths(a=1,b=2)"""
+
+#-----------------CALCULATOR---------------------#
+print("WELCOME TO THE MY CALCULATOR !!")
+print("WHICH MATHS CALCLCULATION YOU NEED TO PERFORM ? ")
+print("1.ADDITION\n2.SUBTRACTION\n3.MULTIPLICATION\n4.DIVISION")
+resp1 = input("enter your operator here : ")
+if resp1 == "1":
+  #-----------------ADDITION---------------------#
+  def addition():
+    a = int(input("enter the first number : "))
+    b = int(input("enter the second number : "))
+    c = (a + b)
+    print(f"THE SUM OF THE TWO NUMBERS IS : {a} and {b} is {c}")
+
+  addition()
+  #in the case if user write "Addition" !
+elif resp1 == "Addition":
+
+  def additioN():
+    a = int(input("enter the first number : "))
+    b = int(input("enter the second number : "))
+    c = (a + b)
+    print(f"THE SUM OF THE TWO NUMBERS IS : {a} and {b} is {c}")
+    additioN()
+
+  #in the case if user write "ADDITION" !
+elif resp1 == "ADDITION":
+
+  def additio():
+    a = int(input("enter the first number : "))
+    b = int(input("enter the second number : "))
+    c = (a + b)
+    print(f"THE SUM OF THE TWO NUMBERS IS : {a} and {b} is {c}")
+    additio()
+
+
+#---------SUBTRACTION---------------------#
+if resp1 == "2":
+
+  def subtration0():
+    a = int(input("enter the first number : "))
+    b = int(input("enter the second number : "))
+    c = (a - b)
+    print(f"THE SUBTRACTION OF THE TWO NUMBERS IS : {a} and {b} is {c}")
+
+  subtration0()
+  #in the if user write "Subtration" !
+elif resp1 == "Subtration":
+
+  def subtration1():
+    a = int(input("enter the first number : "))
+    b = int(input("enter the second number : "))
+    c = (a - b)
+    print(f"THE SUBTRACTION OF THE TWO NUMBERS IS : {a} and {b} is {c}")
+    subtration1()
+    #in the if user write "SUBTRACTION" !
+elif resp1 == "SUBTRACTION":
+
+  def subtration2():
+    a = int(input("enter the first number : "))
+    b = int(input("enter the second number : "))
+    c = (a - b)
+    print(f"THE SUBTRACTION OF THE TWO NUMBERS IS : {a} and {b} is {c}")
+    subtration2()
+    #in the if user write "subtration" !
+elif resp1 == "subtration":
+
+  def subtration3():
+    a = int(input("enter the first number : "))
+    b = int(input("enter the second number : "))
+    c = (a - b)
+    print(f"THE SUBTRACTION OF THE TWO NUMBERS IS : {a} and {b} is {c}")
+    subtration3()
+
+
+#---------MULTIPLICATION---------------------#
+if resp1 == "3":
+
+  def multiplication():
+    a = int(input("enter the first number : "))
+    b = int(input("enter the second number : "))
+    c = (a * b)
+    print(f"THE MULTIPLICATION OF THE TWO NUMBERS IS : {a} and {b} is {c}")
+
+  multiplication()
+  ''' #in the if user write "Multiplication" !
+if resp1 == "Multiplication":
+                      multiplication()
+  #in the if user write "MULTIPLICATION" !
+  if resp1 == "MULTIPLICATION":
+    multiplication()
+  #in the if user write "multiplication" !
+  if resp1 == "multiplication":
+    multiplication() '''
+#in the if user write "Multiplication" !
+if resp1 == "Multiplication":
+
+  def multiplication():
+    a = int(input("enter the first number : "))
+    b = int(input("enter the second number : "))
+    c = (a * b)
+    print(f"THE MULTIPLICATION OF THE TWO NUMBERS IS : {a} and {b} is {c}")
+
+  multiplication()
+  #in the if user write "MULTIPLICATION" !
+if resp1 == "MULTIPLICATION":
+
+  def multiplication():
+    a = int(input("enter the first number : "))
+    b = int(input("enter the second number : "))
+    c = (a * b)
+    print(f"THE MULTIPLICATION OF THE TWO NUMBERS IS : {a} and {b} is {c}")
+
+  multiplication()
+#in the if user write "multiplication" !
+if resp1 == "multiplication":
+
+  def multiplication():
+    a = int(input("enter the first number : "))
+    b = int(input("enter the second number : "))
+    c = (a * b)
+    print(f"THE MULTIPLICATION OF THE TWO NUMBERS IS : {a} and {b} is {c}")
+
+  multiplication()
+
+#---------DIVISION---------------------#
+
+if resp1 == "4":
+
+  def division():
+    a = int(input("enter the first number : "))
+    b = int(input("enter the second number : "))
+    c = (int(a) / int(b))
+    print(f"THE DIVISION OF THE TWO NUMBERS IS : {a} and {b} is {c}")
+
+  division()
+#in the if user write "Division" !
+
+if resp1 == "Division":
+
+  def division():
+    a = int(input("enter the first number : "))
+    b = int(input("enter the second number : "))
+    c = (int(a) / int(b))
+    print(f"THE DIVISION OF THE TWO NUMBERS IS : {a} and {b} is {c}")
+
+  division()
+
+#in the case if user write "DIVISION" !
+
+if resp1 == "DIVISION":
+
+  def division():
+    a = int(input("enter the first number : "))
+    b = int(input("enter the second number : "))
+    c = (int(a) / int(b))
+    print(f"THE DIVISION OF THE TWO NUMBERS IS : {a} and {b} is {c}")
+
+  division()
+
+#in the case if user write "division" !
+
+if resp1 == "division":
+
+  def division():
+    a = int(input("enter the first number : "))
+    b = int(input("enter the second number : "))
+    c = (int(a) / int(b))
+    print(f"THE DIVISION OF THE TWO NUMBERS IS : {a} and {b} is {c}")
+
+  division()
